d277c43b63cf3634e4875c749fad3caef729fa45
tachibana <issei.misasagi.3691@gmail.com>
2023-02-22 01:05:44 +0900

    level3Dの確認テスト

---
+++ TKMK/Game/BackGround.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "ModelRender.h"
+
+class BackGround : public IGameObject
+{
+public:
+	BackGround();
+	~BackGround();
+	bool Start();
+	void Update();
+	void Render(RenderContext& rc);
+
+	void SetPosition(Vector3 position)
+	{
+		m_position = position;
+	}
+	void SetScale(Vector3 scale)
+	{
+		m_scale = scale;
+	}
+	void SetRotation(Quaternion rotation)
+	{
+		m_rotation = rotation;
+	}
+
+private:
+	ModelRender m_modelRender;
+	Vector3 m_position = Vector3::Zero;
+	Vector3 m_scale = Vector3::One;
+	Quaternion m_rotation = Quaternion::Identity;
+	PhysicsStaticObject m_physicsStaticObject;
+
+};
+
--- TKMK/Game/Game.cpp
+++ TKMK/Game/Game.cpp
@@ -1,23 +1,49 @@
 #include "stdafx.h"
 #include "Game.h"
 
+#include "BackGround.h"
+
 Game::Game()
+{
+	
+}
+
+Game::~Game()
+{
+	if (m_backGround != nullptr)
+	{
+		DeleteGO(m_backGround);
+	}
+}
+
+bool Game::Start()
 {
 	m_animationClips[enAnimationClip_Idle].Load("Assets/animData/idle.tka");
 	m_animationClips[enAnimationClip_Idle].SetLoopFlag(true);
 	m_animationClips[enAnimationClip_Walk].Load("Assets/animData/walk.tka");
 	m_animationClips[enAnimationClip_Walk].SetLoopFlag(true);
 
-	m_modelRender.Init("Assets/modelData/unityChan.tkm", enModelUpAxisY, m_animationClips,enAnimationClip_Num);
+	m_modelRender.Init("Assets/modelData/unityChan.tkm", m_animationClips, enAnimationClip_Num, enModelUpAxisY);
 	m_modelRender.SetPosition(Vector3(0.0f, 0.0f, 0.0f));
 	m_modelRender.SetScale(Vector3(1.0f, 1.0f, 1.0f));
 
 	m_rotation.SetRotationY(0.0f);
 	m_modelRender.SetRotation(m_rotation);
-}
 
-Game::~Game()
-{
+	m_level3DRender.Init("Assets/level3D/testLevel.tkl", [&](LevelObjectData& objData) {
+
+		if (objData.EqualObjectName(L"testBackGround") == true) {
+			m_backGround = NewGO<BackGround>(0, "backGround");
+			m_backGround->SetPosition(objData.position);
+			m_backGround->SetRotation(objData.rotation);
+			m_backGround->SetScale(objData.scale);
+
+			return true;
+		}
+		return false;
+	});
+
+	return true;
 }
 
 void Game::Update()

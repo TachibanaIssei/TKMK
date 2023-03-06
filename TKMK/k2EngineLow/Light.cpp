397e519e629749978051c286752cc3592343dcde
tachibana <issei.misasagi.3691@gmail.com>
2023-03-07 00:24:27 +0900

    各種ライトの初期値を調整

--- TKMK/k2EngineLow/Light.cpp
+++ TKMK/k2EngineLow/Light.cpp
@@ -3,21 +3,22 @@
 
 void nsK2EngineLow::SceneLight::Init()
 {
-    m_light.directionalLight.color.x = 1.2f;
-    m_light.directionalLight.color.y = 1.2f;
-    m_light.directionalLight.color.z = 1.2f;
+    m_light.directionalLight.color.x = 0.5f;
+    m_light.directionalLight.color.y = 0.5f;
+    m_light.directionalLight.color.z = 0.5f;
 
     m_light.directionalLight.direction.x = 1.0f;
     m_light.directionalLight.direction.y = -1.0f;
     m_light.directionalLight.direction.z = -1.0f;
     m_light.directionalLight.direction.Normalize();
 
-    m_light.ambientLight.x = 0.1f;
-    m_light.ambientLight.y = 0.1f;
-    m_light.ambientLight.z = 0.1f;
-    m_light.cameraEyePos = g_camera3D->GetPosition();
+    m_light.ambientLight.x = 0.3f;
+    m_light.ambientLight.y = 0.3f;
+    m_light.ambientLight.z = 0.3f;
 
-    m_light.hemisphereLight.groundColor = Vector3(0.7f, 0.5f, 0.3f);
-    m_light.hemisphereLight.skyColor = Vector3(0.15f, 0.7f, 0.95f);
-    m_light.hemisphereLight.groundNormal = Vector3(0.0f, 1.0f, 0.0f);
+    m_light.hemisphereLight.groundColor = Vector3(0.0f, 0.5f, 0.0f);
+    m_light.hemisphereLight.skyColor = Vector3(0.5f, 0.0f, 0.0f);
+    m_light.hemisphereLight.groundNormal = Vector3(1.0f, 0.0f, 0.0f);
+
+    m_light.cameraEyePos = g_camera3D->GetPosition();
 }

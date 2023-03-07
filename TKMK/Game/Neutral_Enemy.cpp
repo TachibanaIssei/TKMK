593fcd3b6f231f64ba3d2e2b7781e30429130885
minmin0304 <1981115290@qq.com>
2023-03-07 14:30:44 +0900

    中立の敵の回転追加

--- TKMK/Game/Neutral_Enemy.cpp
+++ TKMK/Game/Neutral_Enemy.cpp
@@ -70,7 +70,27 @@ void Neutral_Enemy::Update()
 
 void Neutral_Enemy::Rotation()
 {
+	if (fabsf(m_moveSpeed.x) < 0.001f
+		&& fabsf(m_moveSpeed.z) < 0.001f) {
+		//m_moveSpeed.xとm_moveSpeed.zの絶対値がともに0.001以下ということは
+		//このフレームではキャラは移動していないので旋回する必要はない。
+		return;
+	}
+	//atan2はtanθの値を角度(ラジアン単位)に変換してくれる関数。
+	//m_moveSpeed.x / m_moveSpeed.zの結果はtanθになる。
+	//atan2を使用して、角度を求めている。
+	//これが回転角度になる。
+	float angle = atan2(-m_moveSpeed.x, m_moveSpeed.z);
+	//atanが返してくる角度はラジアン単位なので
+	//SetRotationDegではなくSetRotationを使用する。
+	m_rot.SetRotationY(-angle);
 
+	//回転を設定する。
+	m_modelRender.SetRotation(m_rot);
+
+	//プレイヤーの前ベクトルを計算する。
+	m_forward = Vector3::AxisZ;
+	m_rot.Apply(m_forward);
 }
 
 void Neutral_Enemy::Chase()

c9c55b56621172ea73d28f99c3675a73269b1cc0
minmin0304 <1981115290@qq.com>
2023-03-09 14:51:36 +0900

    中立の敵のHPバー追加しました

--- TKMK/Game/GameUI.h
+++ TKMK/Game/GameUI.h
@@ -11,7 +11,9 @@ public:
 	bool Start();
 	void Update();
 	void Render(RenderContext& rc);
-
+	/// HPバーの表示
+/// </summary>
+	void HPBar();
 private:
 	FontRender m_LevelFont;
 	FontRender m_LevelNameFont;
@@ -25,12 +27,11 @@ private:
 	KnightPlayer* m_knightplayer=nullptr;
 	//UI
 	SpriteRender			m_hpBar;														//HPバーの画像
-	SpriteRender			m_mpBar;														//MPバーの画像
 	SpriteRender			m_statusBar;													//ステータスの画像
 	SpriteRender			m_playerFaceFrame;												//プレイヤーの顔画像の枠
 	SpriteRender			m_playerFaceBack;												//プレイヤーの顔画像の背景
-	SpriteRender			m_playerFace;
-
+	FontRender m_time_left;
+	float m_timer = 300.0f;
 	//int LEVEL;
 };
 

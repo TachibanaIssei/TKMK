c0b045abf57f90fe9b893d36f1bbf4676a3328bd
azuki <yuhi55.k@gmail.com>
2023-03-10 23:41:53 +0900

    HPバーの画像追加、HPバー変更、制限時間の処理変更

--- TKMK/Game/GameUI.cpp
+++ TKMK/Game/GameUI.cpp
@@ -5,10 +5,15 @@
 namespace
 {
 	const Vector2 GAUGE_PIVOT = Vector2(0.5f, 0.5f);				//ゲージのピボット
-	const Vector2 HPGAUGE_PIVOT = Vector2(0.5f, 0.5f);				//HPゲージのピボット
-	const Vector3 STATUS_BAR_POS = Vector3(-400.0f, -450.0f, 0.0f);	//ステータスバーポジション
+	const Vector2 HPGAUGE_PIVOT = Vector2(0.0f, 0.5f);				//HPゲージのピボット
+	const Vector3 STATUS_BAR_POS = Vector3(-400.0f, -420.0f, 0.0f);	//ステータスバーポジション
 	const Vector3 PLAYER_FACE_BAR_POS = Vector3(-557.0,-400.0f, 0.0f);	//プレイヤーの顔の枠のポジション
-	const Vector3 HP_BAR_POS = Vector3(-450.0f, -420.0f, 0.0f);	//HPバーポジション
+	const Vector3 HP_BAR_POS = Vector3(-400.0f, -420.0f, 0.0f);	//HPバーポジション
+
+	const float HP_BAR_WIDTH = 1400.0f;     //HPバーの長さ
+	const float HP_BAR_HIGHT = 200.0f;      //HPバーの高さ
+	const Vector3 HP_BAR_SIZE = Vector3(HP_BAR_WIDTH, HP_BAR_HIGHT, 0.0f);  //HPバーのサイズ
+
 	const Vector3 Skill_Pos = Vector3(700.0f, -225.0f, 0.0f);   //スキルアイコンポジション
 	const Vector3 Ult_Pos = Vector3(700.0f, -370.0f, 0.0f);     //必殺技アイコンポジション
 
@@ -29,14 +34,14 @@ bool GameUI::Start()
 	m_knightplayer = FindGO<KnightPlayer>("m_knightplayer");
 
 	//Level
-	m_LevelFont.SetPosition(-800.0f, -200.0f, 0.0f);
+	m_LevelFont.SetPosition(-850.0f, -460.0f, 0.0f);
 	m_LevelFont.SetScale(2.0f);
 	m_LevelFont.SetColor(1.0f, 1.0f, 1.0f, 1.0f);
 	m_LevelFont.SetRotation(0.0f);
 	m_LevelFont.SetShadowParam(true, 2.0f, g_vec4Black);
 
 	m_LevelNameFont.SetText(L"Lv");
-	m_LevelNameFont.SetPosition(-900.0f, -200.0f,0.0f);
+	m_LevelNameFont.SetPosition(-950.0f, -460.0f,0.0f);
 	m_LevelNameFont.SetScale(2.0f);
 	m_LevelNameFont.SetRotation(0.0f);
 	m_LevelNameFont.SetShadowParam(true, 2.0f, g_vec4Black);
@@ -54,18 +59,21 @@ bool GameUI::Start()
 	m_HpNameFont.SetRotation(0.0f);
 	m_HpNameFont.SetShadowParam(true, 2.0f, g_vec4Black);*/
 
-	//HPゲージの画像を読み込む
-	m_statusBar.Init("Assets/sprite/HP_gauge.DDS", 631, 207);
-	m_statusBar.SetPosition(STATUS_BAR_POS);
-	m_statusBar.SetScale(0.87,0.87,1.0);
-	//プレイヤーのアイコン外枠
-	m_playerFaceFrame.Init("Assets/sprite/HP_window.DDS", 602, 197);
-	m_playerFaceFrame.SetPosition(PLAYER_FACE_BAR_POS);
-	m_playerFaceFrame.SetScale(0.8,0.8,1.0);
+
+	//HPゲージ裏の画像を読み込む
+	m_statusBar.Init("Assets/sprite/HP_bar_ura.DDS", 600.0f, 85.0f);
+	m_statusBar.SetPosition(HP_BAR_POS);
+	m_statusBar.SetScale(1.0,1.0,1.0);
+
+	//HPゲージのフレームの画像を読み込む
+	m_playerFaceFrame.Init("Assets/sprite/HP_flame.DDS", 600.0f, 85.0f);
+	m_playerFaceFrame.SetPosition(HP_BAR_POS);
+	m_playerFaceFrame.SetScale(1.0, 1.0, 1.0);
 	//プレイヤーのアイコン
-	m_playerFaceBack.Init("Assets/sprite/HP_windowBack.DDS", 615, 201);
+	/*m_playerFaceBack.Init("Assets/sprite/HP_windowBack.DDS", 615, 201);
 	m_playerFaceBack.SetPosition(PLAYER_FACE_BAR_POS);
-	m_playerFaceBack.SetScale(0.82,0.82,1.0);
+	m_playerFaceBack.SetScale(0.82,0.82,1.0);*/
+
 	//スキルのアイコン
 	m_SkillRender.Init("Assets/sprite/skill_flame.DDS", 162, 162);
 	m_SkillRender.SetPosition(Skill_Pos);
@@ -75,17 +83,18 @@ bool GameUI::Start()
 	m_UltRender.SetPosition(Ult_Pos);
 	m_UltRender.SetScale(1, 1);
 
-	//HPゲージの画像読み込み
-	m_hpBar.Init("Assets/sprite/uf_bar_b_health.DDS", 492.0f, 40.0f);
+	//HPゲージ表の画像読み込み
+	m_hpBar.Init("Assets/sprite/HP_bar.DDS", 600.0f, 85.0f);
 	//ピボットを設定する
 	m_hpBar.SetPivot(HPGAUGE_PIVOT);
-	m_hpBar.SetPosition(HP_BAR_POS);
+	m_hpBar.SetPosition({ -700.0f, -420.0f, 0.0f });
+	m_hpBar.SetScale(0.4f, 0.4f, 1.0f);
 	//更新処理
 	m_statusBar.Update();
 	m_hpBar.Update();
 	//m_hp = PLAYER_MAXHP;
 	m_playerFaceFrame.Update();
-	m_playerFaceBack.Update();
+	//m_playerFaceBack.Update();
 	m_SkillRender.Update();
 	m_UltRender.Update();
 
@@ -100,22 +109,49 @@ void GameUI::Update()
 	m_LevelFont.SetText(Lv);
 
 
+	if (MinutesTimer != 0) {
+		SecondsTimer -= g_gameTime->GetFrameDeltaTime();
+		if (SecondsTimer <= 0) {
+			MinutesTimer--;
+			SecondsTimer = 60.0f;
+		}
+	}
+	
 	wchar_t wcsbuf[256];
-	swprintf_s(wcsbuf, 256, L"残り時間%d", int(m_timer));
-	m_timer-= g_gameTime->GetFrameDeltaTime();
+	swprintf_s(wcsbuf, 256, L"%d:%d", int(MinutesTimer),int(SecondsTimer));
+	
 	//表示するテキストを設定。
 	m_time_left.SetText(wcsbuf);
 	//フォントの設定。
-	m_time_left.SetPosition(Vector3(200.0f, 200.0f, 0.0f));
+	m_time_left.SetPosition(Vector3(-750.0f, -400.0f, 0.0f));
 	//フォントの大きさを設定。
-	m_time_left.SetScale(2.0f);
+	m_time_left.SetScale(1.5f);
 	//フォントの色を設定。
-	m_time_left.SetColor({ 1.0f,0.0f,0.0f,1.0f });
+	m_time_left.SetColor({ 0.0f,0.0f,0.0f,1.0f });
+
+	HPBar();
+	
+
+	
 }
 
 void GameUI::HPBar()
 {
+	Vector3 HpScale = Vector3::One;
+	//HPバーの減っていく割合。
+	HpScale.x = (float)m_knightplayer->SetHp() / (float)m_knightplayer->SetMaxHp();
+	m_hpBar.SetScale(HpScale);
+
+
+
+	//m_HPBerPos.x = -0.0f;
+	////HPバー画像を左寄せに表示する
+	//Vector3 BerSizeSubtraction = HPBerSend(HP_BAR_SIZE, HpScale);	//画像の元の大きさ
+	//m_HPBerPos.x -= BerSizeSubtraction.x;
+
+	//m_hpBar.SetPosition(Vector3(m_HPBerPos.x, HP_BAR_POS.y, 0.0f));
 	
+	m_hpBar.Update();
 }
 void GameUI::Render(RenderContext& rc)
 {
@@ -128,7 +164,7 @@ void GameUI::Render(RenderContext& rc)
 	m_SpeedFont.Draw(rc);*/
 	m_statusBar.Draw(rc);
 	m_hpBar.Draw(rc);
-	m_playerFaceBack.Draw(rc);
+	//m_playerFaceBack.Draw(rc);
 	m_playerFaceFrame.Draw(rc);
 	m_SkillRender.Draw(rc);
 	m_UltRender.Draw(rc);
--- TKMK/Game/GameUI.h
+++ TKMK/Game/GameUI.h
@@ -14,6 +14,25 @@ public:
 	/// HPバーの表示
 /// </summary>
 	void HPBar();
+
+	/// <summary>
+	/// ゲージを左寄せする処理
+	/// </summary>
+	/// <param name="size">画像の元の大きさ</param>
+	/// <param name="scale">現在のスケール倍率</param>
+	/// <returns>変換前と変換後の差</returns>
+	Vector3 HPBerSend(Vector3 size, Vector3 scale)
+	{
+		Vector3 hpBerSize = size;								//画像の元の大きさ
+		Vector3 changeBerSize = Vector3::Zero;					//画像をスケール変換したあとの大きさ
+		Vector3 BerSizeSubtraction = Vector3::Zero;				//画像の元と変換後の差
+
+		changeBerSize.x = hpBerSize.x * scale.x;
+		BerSizeSubtraction.x = hpBerSize.x - changeBerSize.x;
+		BerSizeSubtraction.x /= 2.0f;
+
+		return BerSizeSubtraction;
+	}
 private:
 	FontRender m_LevelFont;
 	FontRender m_LevelNameFont;
@@ -26,16 +45,24 @@ private:
 
 	KnightPlayer* m_knightplayer=nullptr;
 	//UI
-	SpriteRender			m_hpBar;														//HPバーの画像
-	SpriteRender			m_statusBar;													//ステータスの画像
-	SpriteRender			m_playerFaceFrame;												//プレイヤーの顔画像の枠
-	SpriteRender			m_playerFaceBack;												//プレイヤーの顔画像の背景
-	SpriteRender            m_SkillRender;                                                  //スキルアイコン
-	SpriteRender            m_UltRender;                                                    //必殺アイコン
+	SpriteRender			m_hpBar;				//HPバーの画像
+	SpriteRender			m_statusBar;			//ステータスの画像
+	SpriteRender			m_playerFaceFrame;		//プレイヤーの顔画像の枠
+	SpriteRender			m_playerFaceBack;		//プレイヤーの顔画像の背景
+	SpriteRender            m_SkillRender;          //スキルアイコン
+	SpriteRender            m_UltRender;            //必殺アイコン
 	
+	Vector2				m_HPBerPos = Vector2::Zero;				//HPバーのポジション
+	Vector2				m_HPWindowPos = Vector2::Zero;			//HP枠のポジション
+	Vector2				m_HPBackPos = Vector2::Zero;			//HP背景のポジション
 	
 	
 	FontRender m_time_left;
+
+	//秒を計るタイマー
+	float SecondsTimer=0.0f;
+	//分を計るタイマー
+	float MinutesTimer=5.0f;
 	float m_timer = 300.0f;
 	//int LEVEL;
 };

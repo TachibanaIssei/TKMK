6df0be67c85b05ae5c1990b7bec2566a6109649e
tachibana <issei.misasagi.3691@gmail.com>
2023-03-16 15:27:16 +0900

    Bloomを実装した

--- TKMK/k2EngineLow/RenderingEngine.cpp
+++ TKMK/k2EngineLow/RenderingEngine.cpp
@@ -3,7 +3,39 @@
 
 void nsK2EngineLow::RenderingEngine::Init()
 {
+	InitRenderTarget();
+	m_postEffect.InitBloom(m_mainRenderTarget);
+	InitCopyToFrameBufferSprite();
+
 	m_sceneLight.Init();
+}
+
+void nsK2EngineLow::RenderingEngine::InitRenderTarget()
+{
+	//メインレンダリングターゲット
+	m_mainRenderTarget.Create(
+		FRAME_BUFFER_W,
+		FRAME_BUFFER_H,
+		1,
+		1,
+		DXGI_FORMAT_R32G32B32A32_FLOAT,
+		DXGI_FORMAT_D32_FLOAT
+	);
+}
+
+void nsK2EngineLow::RenderingEngine::InitCopyToFrameBufferSprite()
+{
+	SpriteInitData spriteInitData;
+
+	//テクスチャはmainRenderTargetのカラーバッファ
+	spriteInitData.m_textures[0] = &m_mainRenderTarget.GetRenderTargetTexture();
+	spriteInitData.m_width = m_mainRenderTarget.GetWidth();
+	spriteInitData.m_height = m_mainRenderTarget.GetHeight();
+
+	spriteInitData.m_fxFilePath = "Assets/shader/sprite.fx";
+
+	//スプライトを初期化
+	m_copyToFrameBufferSprite.Init(spriteInitData);
 }
 
 void nsK2EngineLow::RenderingEngine::ModelRendering(RenderContext& rc)
@@ -33,8 +65,28 @@ void nsK2EngineLow::RenderingEngine::Execute(RenderContext& rc)
 {
 	SetEyePos(g_camera3D->GetPosition());
 
+	//メインレンダリングターゲットに変更
+	rc.WaitUntilToPossibleSetRenderTarget(m_mainRenderTarget);
+	rc.SetRenderTargetAndViewport(m_mainRenderTarget);
+	rc.ClearRenderTargetView(m_mainRenderTarget);
+
+	//モデルを描画
 	ModelRendering(rc);
+
+	//レンダリングターゲットへの書き込み終了待ち
+	rc.WaitUntilFinishDrawingToRenderTarget(m_mainRenderTarget);
+
+	m_postEffect.Render(rc, m_mainRenderTarget);
+
+	rc.SetRenderTarget(
+		g_graphicsEngine->GetCurrentFrameBuffuerRTV(),
+		g_graphicsEngine->GetCurrentFrameBuffuerDSV()
+	);
+	m_copyToFrameBufferSprite.Draw(rc);
+
+	//スプライトを描画
 	SpriteRendering(rc);
+	//文字を描画
 	FontRendering(rc);
 
 	m_modelList.clear();

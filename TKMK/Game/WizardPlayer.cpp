dfe731b86728c870386857fc577f0a5de4311730
azuki <yuhi55.k@gmail.com>
2023-03-21 00:48:20 +0900

    魔法使いプレイヤークラス作成

---
+++ TKMK/Game/WizardPlayer.cpp
@@ -0,0 +1,78 @@
+#include "stdafx.h"
+#include "WizardPlayer.h"
+#include "Game.h"
+#include "Neutral_Enemy.h"
+
+WizardPlayer::WizardPlayer()
+{
+	//魔法使いのモデルを読み込む
+	SetModel();
+
+	//アニメーションイベント用の関数を設定する。
+	m_modelRender.AddAnimationEvent([&](const wchar_t* clipName, const wchar_t* eventName) {
+		OnAnimationEvent(clipName, eventName);
+		});
+	//リスポーンする座標2番の取得
+	GetRespawnPos();
+	respawnNumber = 2;        //リスポーンする座標の番号
+
+	//リスポーンする座標のセット
+	//キャラコン
+	m_charCon.SetPosition(m_respawnPos[respawnNumber]);
+	//剣士
+	m_modelRender.SetPosition(m_respawnPos[respawnNumber]);
+
+	//m_position=m_respawnPos[respawnNumber];
+
+	m_position = m_charCon.Execute(m_moveSpeed, 1.0f / 60.0f);
+
+	//剣士のY座標が腰なのでY座標を上げる
+	//m_position.y = m_position_YUp;
+
+	m_modelRender.SetPosition(m_position);
+	//m_modelRender.Update();
+}
+
+WizardPlayer::~WizardPlayer()
+{
+
+}
+
+void WizardPlayer::Update()
+{
+	//gameクラスのポーズのフラグが立っている間処理を行わない
+	if (m_wizardState == enWizardState_Pause) {
+		return;
+	}
+
+	//移動処理
+	Move(m_position, m_charCon, m_Status);
+
+	//回転処理
+	Rotation();
+
+	//ステート
+	ManageState();
+	//アニメーションの再生
+	PlayAnimation();
+
+
+	m_modelRender.SetPosition(m_position);
+	m_modelRender.Update();
+}
+
+void WizardPlayer::Attack()
+{
+
+}
+
+void WizardPlayer::OnAnimationEvent(const wchar_t* clipName, const wchar_t* eventName)
+{
+
+}
+
+void WizardPlayer::Render(RenderContext& rc)
+{
+	m_modelRender.Draw(rc);
+}
+
---
+++ TKMK/Game/WizardPlayer.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "WizardBase.h"
+
+#include "Status.h"
+
+class Game;
+class Neutral_Enemy;
+
+class WizardPlayer:public WizardBase
+{
+public:
+	WizardPlayer();
+	~WizardPlayer();
+
+	void Update();
+	void Attack();
+	void Render(RenderContext& rc);
+
+	void OnAnimationEvent(const wchar_t* clipName, const wchar_t* eventName);
+
+	inline Vector3 GetPosition() { return m_position; }
+
+	//void AvoidanceSprite();
+
+
+
+
+
+private:
+	Game* m_game = nullptr;
+	Neutral_Enemy* m_Neutral_Enemy = nullptr; //’†—§‚Ì“G
+
+
+
+};
+

ad26f5219f93dfe2da1554aa94f12b470f1dbd26
azuki <yuhi55.k@gmail.com>
2023-03-22 23:00:09 +0900

    剣のコリジョンを作成する関数をベースからプレイヤーに移した。AIのコリジョンのif文をコメントアウト

--- TKMK/Game/KnightBase.cpp
+++ TKMK/Game/KnightBase.cpp
@@ -145,27 +145,27 @@ void KnightBase::Rotation()
 /// <summary>
 /// 攻撃時の当たり判定の処理
 /// </summary>
-void KnightBase::AtkCollisiton()
-{
-	//コリジョンオブジェクトを作成する。
-	auto collisionObject = NewGO<CollisionObject>(0);
-	Vector3 collisionPosition = m_position;
-	//座標をプレイヤーの少し前に設定する。
-	//collisionPosition += forward * 50.0f;
-	//ボックス状のコリジョンを作成する。
-	collisionObject->CreateBox(collisionPosition, //座標。
-		Quaternion::Identity, //回転。
-		Vector3(70.0f, 15.0f, 15.0f) //大きさ。
-	);
-	collisionObject->SetName("player_attack");
-
-	//「Sword」ボーンのワールド行列を取得する。
-	Matrix matrix = m_modelRender.GetBone(m_swordBoneId)->GetWorldMatrix();
-
-	//matrix.MakeRotationZ(90.0f);
-	//「Sword」ボーンのワールド行列をコリジョンに適用する。
-	collisionObject->SetWorldMatrix(matrix);
-}
+//void KnightBase::AtkCollisiton()
+//{
+//	//コリジョンオブジェクトを作成する。
+//	auto collisionObject = NewGO<CollisionObject>(0);
+//	Vector3 collisionPosition = m_position;
+//	//座標をプレイヤーの少し前に設定する。
+//	//collisionPosition += forward * 50.0f;
+//	//ボックス状のコリジョンを作成する。
+//	collisionObject->CreateBox(collisionPosition, //座標。
+//		Quaternion::Identity, //回転。
+//		Vector3(70.0f, 15.0f, 15.0f) //大きさ。
+//	);
+//	collisionObject->SetName("player_attack");
+//
+//	//「Sword」ボーンのワールド行列を取得する。
+//	Matrix matrix = m_modelRender.GetBone(m_swordBoneId)->GetWorldMatrix();
+//
+//	//matrix.MakeRotationZ(90.0f);
+//	//「Sword」ボーンのワールド行列をコリジョンに適用する。
+//	collisionObject->SetWorldMatrix(matrix);
+//}
 
 /// <summary>
 /// 必殺技発動時の当たり判定の処理

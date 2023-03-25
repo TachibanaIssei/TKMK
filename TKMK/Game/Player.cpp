9d8bc89c01ae1bcb28a9548fdbff21466dac27b9
azuki <yuhi55.k@gmail.com>
2023-03-25 16:00:50 +0900

    関数を.hから.cppに移した

--- TKMK/Game/Player.cpp
+++ TKMK/Game/Player.cpp
@@ -49,4 +49,228 @@ bool Player::Start()
 	return true;
 }
 
+/// <summary>
+	/// 生成するキャラを選ぶ
+	/// </summary>
+	/// <param name="number">キャラの番号</param>
+void Player::CharSelect(int number)
+{
+	switch (number)
+	{
+	case 0:
+		m_selectCharctar = enKnight;
+		break;
+	case 1:
+		m_selectCharctar = enWizard;
+		break;
+	case 2:
+		m_selectCharctar = enZombie;
+		break;
+	case 3:
+		m_selectCharctar = enMonster;
+		break;
+
+	default:
+		break;
+	}
+}
+
+/// <summary>
+/// キャラの座標を取得
+/// </summary>
+/// <returns>選択されているキャラの座標</returns>
+Vector3 Player::GetCharPosition()const
+{
+	switch (m_selectCharctar)
+	{
+	case enKnight:
+		return knightPlayer->GetPosition();
+		break;
+
+	case enWizard:
+		return wizardPlayer->GetPosition();
+		break;
+
+	case enZombie:
+		break;
+
+	case enMonster:
+		break;
+
+	default:
+		break;
+	}
+}
+
+/// <summary>
+/// キャラの現在のレベルを返す
+/// </summary>
+/// <returns>選択されたキャラの現在のレベルを返す関数</returns>
+int Player::CharSetLevel()const
+{
+	switch (m_selectCharctar)
+	{
+	case enKnight:
+		return knightPlayer->SetLevel();
+		break;
+	case enWizard:
+		return wizardPlayer->SetLevel();
+		break;
+	case enZombie:
+		break;
+	case enMonster:
+		break;
+
+	default:
+		break;
+	}
+}
+
+/// <summary>
+/// キャラの現在のヒットポイントを返す
+/// </summary>
+/// <returns>選択されたキャラの現在のヒットポイントを返す関数</returns>
+int Player::CharSetHp()const
+{
+	switch (m_selectCharctar)
+	{
+	case enKnight:
+		return knightPlayer->SetHp();
+		break;
+
+	case enWizard:
+		return wizardPlayer->SetHp();
+		break;
+
+	case enZombie:
+		break;
+
+	case enMonster:
+		break;
+
+
+	default:
+		break;
+	}
+}
+
+/// <summary>
+/// キャラの最大ヒットポイントを返す
+/// </summary>
+/// <returns>選択されたキャラの最大ヒットポイントを返す関数</returns>
+int Player::CharSetMaxHp()const
+{
+	switch (m_selectCharctar)
+	{
+	case enKnight:
+		return knightPlayer->SetMaxHp();
+		break;
+
+	case enWizard:
+		return wizardPlayer->SetMaxHp();
+		break;
+
+	case enZombie:
+		break;
+
+	case enMonster:
+		break;
+
+
+	default:
+		break;
+	}
+}
+
+/// <summary>
+/// キャラの回転量を返す
+/// </summary>
+/// <returns>選択されたキャラの回転量を返す関数</returns>
+Quaternion Player::CharSetRot()const
+{
+	switch (m_selectCharctar)
+	{
+	case enKnight:
+		return knightPlayer->GetRot();
+		break;
+
+	case enWizard:
+		return wizardPlayer->GetRot();
+		break;
+
+	case enZombie:
+		break;
+
+	case enMonster:
+		break;
+
+
+	default:
+		break;
+	}
+}
+
+/// <summary>
+/// キャラのステートを変更する
+/// </summary>
+/// <returns>選択されたキャラのステートを変更する関数</returns>
+void Player::CharSetState(EnPlayerStateChange gameState)const
+{
+	switch (m_selectCharctar)
+	{
+	case enKnight:
+		//ゲームのステートがポーズなら
+		if (gameState == enPause)knightPlayer->SetPlayerState(KnightPlayer::enKnightState_Pause);
+		//ゲームのステートがゲームなら
+		if (gameState == enGame)knightPlayer->SetPlayerState(KnightPlayer::enKnightState_Idle);
+		break;
+
+	case enWizard:
+
+		//ゲームのステートがポーズなら
+		if (gameState == enPause)wizardPlayer->SetPlayerState(WizardPlayer::enWizardState_Pause);
+		//ゲームのステートがゲームなら
+		if (gameState == enGame)wizardPlayer->SetPlayerState(WizardPlayer::enWizardState_Idle);
+		break;
+
+	case enZombie:
+		break;
+
+	case enMonster:
+		break;
+
+
+	default:
+		break;
+	}
+}
+
+/// <summary>
+/// キャラの前方向を取得する
+/// </summary>
+/// <returns>選択されたキャラの前方向を取得する関数</returns>
+Vector3 Player::CharSetForward()const
+{
+	switch (m_selectCharctar)
+	{
+	case enKnight:
+		return knightPlayer->GetForward();
+		break;
+
+	case enWizard:
+		return wizardPlayer->GetForward();
+		break;
+
+	case enZombie:
+		break;
+
+	case enMonster:
+		break;
+
+
+	default:
+		break;
+	}
+}
+
 
--- TKMK/Game/Player.h
+++ TKMK/Game/Player.h
@@ -38,197 +38,49 @@ public:
 	/// 生成するキャラを選ぶ
 	/// </summary>
 	/// <param name="number">キャラの番号</param>
-	void CharSelect(int number)
-	{
-		switch (number)
-		{
-		case 0:
-			m_selectCharctar = enKnight;
-			break;
-		case 1:
-			m_selectCharctar = enWizard;
-			break;
-		case 2:
-			m_selectCharctar = enZombie;
-			break;
-		case 3:
-			m_selectCharctar = enMonster;
-			break;
-
-		default:
-			break;
-		}
-	}
+	void CharSelect(int number);
 
 	/// <summary>
 	/// キャラの座標を取得
 	/// </summary>
 	/// <returns>選択されているキャラの座標</returns>
-	Vector3 GetCharPosition()const
-	{
-		switch (m_selectCharctar)
-		{
-		case enKnight:
-			return knightPlayer->GetPosition();
-			break;
-
-		case enWizard:
-			return wizardPlayer->GetPosition();
-			break;
-
-		case enZombie:
-			break;
-
-		case enMonster:
-			break;
-
-		default:
-			break;
-		}
-	}
+	Vector3 GetCharPosition()const;
 
 	/// <summary>
 	/// キャラの現在のレベルを返す
 	/// </summary>
 	/// <returns>選択されたキャラの現在のレベルを返す関数</returns>
-	int CharSetLevel()const
-	{
-		switch (m_selectCharctar)
-		{
-		case enKnight:
-			return knightPlayer->SetLevel();
-			break;
-		case enWizard:
-			return wizardPlayer->SetLevel();
-			break;
-		case enZombie:
-			break;
-		case enMonster:
-			break;
-		
-		default:
-			break;
-		}
-	}
+	int CharSetLevel()const;
 
 	/// <summary>
 	/// キャラの現在のヒットポイントを返す
 	/// </summary>
 	/// <returns>選択されたキャラの現在のヒットポイントを返す関数</returns>
-	int CharSetHp()const
-	{
-		switch (m_selectCharctar)
-		{
-		case enKnight:
-			return knightPlayer->SetHp();
-			break;
-
-		case enWizard:
-			return wizardPlayer->SetHp();
-			break;
-
-		case enZombie:
-			break;
-
-		case enMonster:
-			break;
-
-
-		default:
-			break;
-		}
-	}
+	int CharSetHp()const;
 
 	/// <summary>
 	/// キャラの最大ヒットポイントを返す
 	/// </summary>
 	/// <returns>選択されたキャラの最大ヒットポイントを返す関数</returns>
-	int CharSetMaxHp()const
-	{
-		switch (m_selectCharctar)
-		{
-		case enKnight:
-			return knightPlayer->SetMaxHp();
-			break;
-
-		case enWizard:
-			return wizardPlayer->SetMaxHp();
-			break;
-
-		case enZombie:
-			break;
-
-		case enMonster:
-			break;
-
-
-		default:
-			break;
-		}
-	}
+	int CharSetMaxHp()const;
 
 	/// <summary>
 	/// キャラの回転量を返す
 	/// </summary>
 	/// <returns>選択されたキャラの回転量を返す関数</returns>
-	Quaternion CharSetRot()const
-	{
-		switch (m_selectCharctar)
-		{
-		case enKnight:
-			return knightPlayer->GetRot();
-			break;
-
-		case enWizard:
-			return wizardPlayer->GetRot();
-			break;
-
-		case enZombie:
-			break;
-
-		case enMonster:
-			break;
-
-
-		default:
-			break;
-		}
-	}
+	Quaternion CharSetRot()const;
 
 	/// <summary>
 	/// キャラのステートを変更する
 	/// </summary>
 	/// <returns>選択されたキャラのステートを変更する関数</returns>
-	void CharSetState(EnPlayerStateChange gameState)const
-	{
-		switch (m_selectCharctar)
-		{
-		case enKnight:
-			//ゲームのステートがポーズなら
-			if(gameState==enPause)knightPlayer->SetPlayerState(KnightPlayer::enKnightState_Pause);
-			//ゲームのステートがゲームなら
-			if (gameState == enGame)knightPlayer->SetPlayerState(KnightPlayer::enKnightState_Idle);
-			break;
-
-		case enWizard:
-			
-			//ゲームのステートがポーズなら
-			if (gameState == enPause)wizardPlayer->SetPlayerState(WizardPlayer::enWizardState_Pause);
-			//ゲームのステートがゲームなら
-			if (gameState == enGame)wizardPlayer->SetPlayerState(WizardPlayer::enWizardState_Idle);
-			break;
-
-		case enZombie:
-			break;
-
-		case enMonster:
-			break;
-
-
-		default:
-			break;
-		}
-	}
+	void CharSetState(EnPlayerStateChange gameState)const;
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
+	Vector3 CharSetForward()const;
 
 private:
 

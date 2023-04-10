9b51b67aa6746d75df0985f192a3371466b912fc
Maekawa Rei <whoi6273@gmail.com>
2023-04-10 15:12:52 +0900

    タイトル画面でセレクト（はじめる、操作説明、キャラクター説明）を選べるようにした

--- TKMK/Game/Tittle.cpp
+++ TKMK/Game/Tittle.cpp
@@ -23,6 +23,27 @@ bool Tittle::Start()
 	m_spriteRender.SetRotation(m_sRotation);
 	m_spriteRender.Update();
 
+	//Select‚Ì‰Šú‰»
+	m_start.Init("Assets/sprite/Start.DDS", 200.0f, 100.0f);
+	m_start.SetPosition(0.0f, -150.0f, 0.0f);
+	m_start.SetScale(1.0f, 1.0f, 1.0f);
+	m_start.Update();
+
+	m_operation.Init("Assets/sprite/Operation.DDS", 300.0f, 100.0f);
+	m_operation.SetPosition(0.0f, -250.0f, 0.0f);
+	m_operation.SetScale(0.7f, 1.0f, 1.0f);
+	m_operation.Update();
+
+	m_charaExplanation.Init("Assets/sprite/CharaExplanation.DDS", 200.0f, 100.0f);
+	m_charaExplanation.SetPosition(0.0f, -350.0f, 0.0f);
+	m_charaExplanation.SetScale(1.7f, 1.7f, 1.0f);
+	m_charaExplanation.Update();
+
+	m_choice.Init("Assets/sprite/Choice.DDS", 200.0f, 100.0f);
+	m_choice.SetPosition(m_position);
+	m_choice.SetScale(1.7f, 1.0f, 1.0f);
+	m_choice.Update();
+
 	//BGM‚ÌÝ’è
 	g_soundEngine->ResistWaveFileBank(1, "Assets/sound/gameBGM/TitleBGM1.wav");
 	//‘I‘ð‰¹
@@ -39,22 +60,72 @@ bool Tittle::Start()
 void Tittle::Update()
 {
 	//ƒQ[ƒ€‰æ–Ê‚Ö‚Ì‘JˆÚ
-	if (g_pad[0]->IsTrigger(enButtonA)) {
+	if (g_pad[0]->IsTrigger(enButtonA) && m_tSelectPosition == enSelectPosition_Start) {
 		//‘I‘ð‰¹
-		m_se = NewGO<SoundSource>(0);
-		m_se->Init(5);
-		m_se->Play(false);
-		m_se->SetVolume(1.0f);
+		SoundSource* se = NewGO<SoundSource>(0);
+		se->Init(5);
+		se->Play(false);
+		se->SetVolume(1.0f);
 		//game‰æ–Ê‚Ö‘JˆÚ
 		Game* game = NewGO<Game>(0, "game");
 		DeleteGO(this);
 		DeleteGO(m_bgm);
 	}
 
+	Select();
+
 	m_spriteRender.Update();
+	m_choice.Update();
+	m_start.Update();
+	m_operation.Update();
+	m_charaExplanation.Update();
+	
+}
+void Tittle::Select()
+{
+	if (g_pad[0]->IsTrigger(enButtonUp))
+	{
+		if(selectPosition !=0)
+			selectPosition--;
+		SoundSource* se = NewGO<SoundSource>(0);
+		se->Init(5);
+		se->Play(false);
+		se->SetVolume(1.0f);
+	}
+	if (g_pad[0]->IsTrigger(enButtonDown))
+	{
+		if(selectPosition <2)
+			selectPosition++;
+		SoundSource* se = NewGO<SoundSource>(0);
+		se->Init(5);
+		se->Play(false);
+		se->SetVolume(1.0f);
+	}
+
+	switch (selectPosition)
+	{
+	case 0:
+		m_tSelectPosition = enSelectPosition_Start;
+		m_position = { 0.0f, -150.0f, 0.0f };
+		break;
+	case 1:
+		m_tSelectPosition = enSelectPosition_Operation;
+		m_position = { 0.0f, -250.0f, 0.0f };
+		break;
+	case 2:
+		m_tSelectPosition = enSelectPosition_CharaExplanation;
+		m_position = { 0.0f, -350.0f, 0.0f };
+		break;
+	}
+	m_choice.SetPosition(m_position);
+	m_choice.Update();
 }
 
 void Tittle::Render(RenderContext& rc)
 {
 	m_spriteRender.Draw(rc);
+	m_choice.Draw(rc);
+	m_start.Draw(rc);
+	m_operation.Draw(rc);
+	m_charaExplanation.Draw(rc);
 }
--- TKMK/Game/Tittle.h
+++ TKMK/Game/Tittle.h
@@ -10,12 +10,28 @@ public:
 	bool Start();
 	void Update();
 	void Render(RenderContext& rc);
+	void Select();
 
+	enum EnSelectPosition {
+		enSelectPosition_Start,						//スタートポジション
+		enSelectPosition_Operation,					//操作説明のポジション
+		enSelectPosition_CharaExplanation,			//キャラ説明のポジション
+		enSelectPosition_Num,						//セレクトの数
+	};
+
+private:
 	Quaternion m_sRotation;
 	SpriteRender m_spriteRender;
+	SpriteRender m_start;
+	SpriteRender m_operation;
+	SpriteRender m_charaExplanation;
+	SpriteRender m_choice;
+	EnSelectPosition m_tSelectPosition;
 	Game* game=nullptr;
 	SoundSource* m_bgm = nullptr;
-	SoundSource* m_se = nullptr;
 
+	Vector3 m_position = { 0.0f,-150.0f,0.0f };
+
+	int selectPosition = 0;
 };
 

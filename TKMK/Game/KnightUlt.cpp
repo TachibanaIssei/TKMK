7446dbe0647d4b1c01097692f5595224e43a4995
azuki <yuhi55.k@gmail.com>
2023-04-21 00:55:12 +0900

    モデル生成。

--- TKMK/Game/KnightUlt.cpp
+++ TKMK/Game/KnightUlt.cpp
@@ -3,6 +3,7 @@
 
 namespace {
 	const Vector3 CollsionSize = Vector3(300.0f, 120.0f, 15.0f);
+	const float SphereSize = 20.0f;
 }
 
 KnightUlt::KnightUlt()
@@ -13,6 +14,7 @@ KnightUlt::KnightUlt()
 KnightUlt::~KnightUlt()
 {
 	DeleteGO(UltCollision);
+	DeleteGO(UltDeleteJudgeCollision);
 }
 
 bool KnightUlt::Start()
@@ -21,12 +23,26 @@ bool KnightUlt::Start()
 	m_moveSpeed = Vector3::AxisZ;
 	m_rotation.Apply(m_moveSpeed);
 	m_position += m_moveSpeed * 20.0f;
-	m_moveSpeed *= 200.0f;
+	m_moveSpeed *= 180.0f;
 	m_rotation.AddRotationDegY(360.0f);
 
 	SetCollision(CollsionSize);
 
-	
+	model.Init("Assets/modelData/character/Knight/Knight_Ult.tkm");
+	model.SetPosition(m_position);
+	model.SetRotation(m_rotation);
+	model.Update();
+
+
+	//m_position_judge = m_moveSpeed * 20.0f;
+
+	//コリジョンオブジェクトを作成する。
+	UltDeleteJudgeCollision = NewGO<CollisionObject>(0);
+	UltDeleteJudgeCollision->CreateSphere(m_position, m_rotation, SphereSize);
+	//コリジョンの名前
+	UltDeleteJudgeCollision->SetName("judgeCollision");
+	//懲り所オブジェクトが自動で削除されないようにする。
+	UltDeleteJudgeCollision->SetIsEnableAutoDelete(false);
 
 	return true;
 }
@@ -36,9 +52,14 @@ void KnightUlt::Update()
 	//座標を移動させる。
 	m_position += m_moveSpeed * g_gameTime->GetFrameDeltaTime();
 
-	//コリジョンオブジェクトに座標を設定する。
-	UltCollision->SetPosition(m_position);
+	m_position_judge = m_position;
+	//前方向
+	m_forward = m_moveSpeed;
 
+	//コリジョンオブジェクトに座標を設定する
+	UltCollision->SetPosition(m_position);
+	//壁判定用の当たり判定の座標を設定する
+	UltDeleteJudgeCollision->SetPosition(m_position);
 	//タイマーを加算する。
 	m_timer += g_gameTime->GetFrameDeltaTime();
 
@@ -47,5 +68,16 @@ void KnightUlt::Update()
 		DeleteGO(this);
 	}
 
+
+	model.SetPosition(m_position);
+	model.Update();
+
+
 	UltCollision->Update();
+	UltDeleteJudgeCollision->Update();
+}
+
+void KnightUlt::Render(RenderContext& rc)
+{
+	model.Draw(rc);
 }

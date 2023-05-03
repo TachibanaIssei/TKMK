5d54190166dfe907431b3bad2b360c661cd20ad3
azuki <yuhi55.k@gmail.com>
2023-05-03 12:27:54 +0900

    キャラクター選択画面変更。モデルを表示して回転させた

--- TKMK/Game/CharacterSelect.cpp
+++ TKMK/Game/CharacterSelect.cpp
@@ -3,6 +3,16 @@
 #include "Tittle.h"
 #include "Game.h"
 
+namespace {
+	const Vector3 StatusPos = Vector3(400.0f, 250.0f, 0.0f);			//ステータスの座標
+	const Vector3 Attack_IconPos = Vector3(400.0f, -150.0f, 0.0f);		//攻撃、スキル、必殺技アイコンの座標
+	const Vector3 NamePos = Vector3(-600.0f, -400.0f, 0.0f);				//名前の座標
+	const Vector3 UnderBarPos = Vector3(0.0f, -500.0f, 0.0f);			//画面下のバーの座標
+
+	const Vector3 KnightPOs = Vector3(-600.0f, 0.0f, 0.0f);
+	const Quaternion KnightRot = Quaternion(0.0f, 90.0f, 0.0f, 1.0f);
+}
+
 CharacterSelect::CharacterSelect()
 {
 
@@ -15,6 +25,22 @@ CharacterSelect::~CharacterSelect()
 
 bool CharacterSelect::Start()
 {
+	
+
+	Quaternion rot/*={0.0f,0.0f,0.0f,0.0f}*/;
+	//rot.x = 10.0f;
+	rot.Apply(m_CameraPosition);
+	g_camera3D->SetPosition(m_CameraPosition);
+	//g_camera3D->
+
+	g_renderingEngine->SetAmbient(Vector4(0.5f, 0.5f, 0.5f, 1.0f));
+	Vector3 dir = { 0.0f,-1.0f,0.0f };
+	Vector4 color = { 0.5f,0.5f,0.5f,1.0f };
+	g_renderingEngine->SetDirectionLight(0, dir, color);
+
+	//剣士のモデル、アニメーション
+	SetKnightModel();
+
 	//キャラクターセレクト画面
 	m_CharacterSelectPic.Init("Assets/sprite/CharacterSelectPic.DDS", 1920.0f, 1080.0f);
 	m_CharacterSelectPic.SetPosition(0.0f, 0.0f, 0.0f);
@@ -38,6 +64,31 @@ bool CharacterSelect::Start()
 	m_SelectCursor.SetPosition(curPosition);
 	m_SelectCursor.SetScale(1.0f, 1.0f, 1.0f);
 	m_SelectCursor.Update();
+
+	//////////////////////////////////////////////////////////////////////////////////////
+	//ステータス
+	m_Status.Init("Assets/sprite/Select/Status.DDS", 950.0f, 350.0f);
+	m_Status.SetPosition(StatusPos);
+	m_Status.SetScale(1.0f, 1.0f, 1.0f);
+	m_Status.Update();
+
+	//攻撃、スキル、必殺技アイコン
+	m_Attack_Icon.Init("Assets/sprite/Select/Attack_Icon.DDS", 1000.0f, 450.0f);
+	m_Attack_Icon.SetPosition(Attack_IconPos);
+	m_Attack_Icon.SetScale(1.0f, 1.0f, 1.0f);
+	m_Attack_Icon.Update();
+
+	//名前
+	m_name.Init("Assets/sprite/Select/name.DDS", 550.0f, 200.0f);
+	m_name.SetPosition(NamePos);
+	m_name.SetScale(1.0f, 1.0f, 1.0f);
+	m_name.Update();
+
+	//画面下のバー
+	m_UnderBar.Init("Assets/sprite/Select/underBar.DDS", 1920.0f, 300.0f);
+	m_UnderBar.SetPosition(UnderBarPos);
+	m_UnderBar.SetScale(1.0f, 1.0f, 1.0f);
+	m_UnderBar.Update();
 
 	return true;
 }
@@ -83,6 +134,10 @@ void CharacterSelect::Update()
 	m_KnightPic.Update();
 	m_WizardPic.Update();
 	m_SelectCursor.Update();
+
+	m_KnightRot.AddRotationDegY(4.0f);
+	m_Knight.SetRotation(m_KnightRot);
+	m_Knight.Update();
 }
 
 void CharacterSelect::Cursor()
@@ -152,11 +207,58 @@ void CharacterSelect::Cursor()
 	m_SelectCursor.Update();
 }
 
+void CharacterSelect::SetKnightModel()
+{
+	m_animationClips[enAnimationClip_Idle].Load("Assets/animData/Knight/Knight_idle.tka");
+	m_animationClips[enAnimationClip_Idle].SetLoopFlag(true);
+	m_animationClips[enAnimationClip_Walk].Load("Assets/animData/Knight/Knight_Walk.tka");
+	m_animationClips[enAnimationClip_Walk].SetLoopFlag(true);
+	m_animationClips[enAnimationClip_Run].Load("Assets/animData/Knight/run.tka");
+	m_animationClips[enAnimationClip_Run].SetLoopFlag(true);
+	m_animationClips[enAnimationClip_ChainAtk].Load("Assets/animData/Knight/ChainAttack/first_Attack.tka");
+	m_animationClips[enAnimationClip_ChainAtk].SetLoopFlag(false);
+	m_animationClips[enAnimationClip_SecondAtk].Load("Assets/animData/Knight/ChainAttack/Second_Attack.tka");
+	m_animationClips[enAnimationClip_SecondAtk].SetLoopFlag(false);
+	m_animationClips[enAnimationClip_lastAtk].Load("Assets/animData/Knight/ChainAttack/last_Attack.tka");
+	m_animationClips[enAnimationClip_lastAtk].SetLoopFlag(false);
+	m_animationClips[enAnimationClip_Skill].Load("Assets/animData/Knight/Knight_Skill.tka");
+	m_animationClips[enAnimationClip_Skill].SetLoopFlag(false);
+	m_animationClips[enAnimationClip_UltimateSkill].Load("Assets/animData/Knight/Knight_UltimateAttack.tka");
+	m_animationClips[enAnimationClip_UltimateSkill].SetLoopFlag(false);
+	m_animationClips[enAnimationClip_Damege].Load("Assets/animData/Knight/Knight_Damege.tka");
+	m_animationClips[enAnimationClip_Damege].SetLoopFlag(false);
+	m_animationClips[enAnimationClip_Death].Load("Assets/animData/Knight/Knight_Death.tka");
+	m_animationClips[enAnimationClip_Death].SetLoopFlag(false);
+	m_animationClips[enAnimationClip_Avoidance].Load("Assets/animData/Knight/Knight_Avoidance.tka");
+	m_animationClips[enAnimationClip_Avoidance].SetLoopFlag(false);
+	m_animationClips[enAnimationClip_Jump].Load("Assets/animData/Knight/Knight_Jump.tka");
+	m_animationClips[enAnimationClip_Jump].SetLoopFlag(false);
+	m_animationClips[enAnimationClip_Fall].Load("Assets/animData/Knight/Knight_fall2.tka");
+	m_animationClips[enAnimationClip_Fall].SetLoopFlag(true);
+
+	//剣士モデルを読み込み
+	m_Knight.Init("Assets/modelData/character/Knight/Knight_02.tkm", m_animationClips, enAnimationClip_Num, enModelUpAxisZ);
+	m_Knight.SetPosition(-90.0f, 0.0f, 0.0f);
+	m_Knight.SetScale(1.5f, 1.5f, 1.5f);
+	/*Quaternion Krot;
+	Krot.y = 90.0f;
+	m_Knight.SetRotation(Krot);*/
+	m_Knight.Update();
+}
+
 void CharacterSelect::Render(RenderContext& rc)
 {
-	m_CharacterSelectPic.Draw(rc);
-	m_KnightPic.Draw(rc);
-	m_WizardPic.Draw(rc);
-	m_SelectCursor.Draw(rc);
+	//m_CharacterSelectPic.Draw(rc);
+	//m_KnightPic.Draw(rc);
+	//m_WizardPic.Draw(rc);
+	//m_SelectCursor.Draw(rc);
+
+	//剣士のモデル
+	m_Knight.Draw(rc);
+
+	m_Status.Draw(rc);
+	m_Attack_Icon.Draw(rc);
+	m_name.Draw(rc);
+	m_UnderBar.Draw(rc);
 }
 
--- TKMK/Game/CharacterSelect.h
+++ TKMK/Game/CharacterSelect.h
@@ -11,8 +11,30 @@ public:
 	bool Start();
 	void Update();
 	void Cursor();
+
+	void SetKnightModel();
+
 	void Render(RenderContext& rc);
 	
+	//アニメーション
+	enum EnAnimationClip {
+		enAnimationClip_Idle,
+		enAnimationClip_Walk,
+		enAnimationClip_Run,
+		enAnimationClip_ChainAtk,
+		enAnimationClip_SecondAtk,
+		enAnimationClip_lastAtk,
+		enAnimationClip_Damege,
+		enAnimationClip_Death,
+		enAnimationClip_Skill,
+		enAnimationClip_UltimateSkill,
+		enAnimationClip_Avoidance,
+		enAnimationClip_Jump,
+		enAnimationClip_Fall,
+		enAnimationClip_Num,
+	};
+	AnimationClip m_animationClips[enAnimationClip_Num]; //アニメーションクリップ
+
 	enum EnCharacterSelect {
 		enCharacterSelect_Knight,		//剣士
 		enCharacterSelect_Wizard,		//魔法使い
@@ -28,9 +50,20 @@ private:
 	SpriteRender m_KnightPic;				//剣士の画像
 	SpriteRender m_WizardPic;				//魔法使いの画像
 
+	SpriteRender m_Status;					//ステータス
+	SpriteRender m_Attack_Icon;				//攻撃、スキル、必殺技アイコン
+	SpriteRender m_UnderBar;				//画面下のバー
+	SpriteRender m_name;					//名前
+
+	ModelRender m_Knight;					//剣士のモデル
+	Quaternion m_KnightRot;
+	
+
 	EnCharacterSelect m_characterSelect;	//キャラクターセレクト
 
 	Vector3 curPosition = { -510.0f,75.0f,0.0f };
+
+	Vector3 m_CameraPosition = { -30.0f, 50.0f, -150.0f };
 
 	int CursorNum = 0;
 	int TitleScene = 1;

16827718bc4f64de3b650af62e5519cc40a1763e
CA01972018 <1981115290@qq.com>
2023-05-04 15:20:12 +0900

    エフェクト専用のところ追加

--- TKMK/Game/Effect.h
+++ TKMK/Game/Effect.h
@@ -1,16 +1,17 @@
 #pragma once
 
 enum EnEFK {
-	enEffect_Knight_Death_Red,
-	enEffect_Knight_Death_Green,
-	enEffect_Knight_Death_Blue,
-	enEffect_Knight_Death_Yellow,
+	enEffect_Knight_Death,
 	enEffect_Knight_Ult_Red,
 	enEffect_Knight_Ult_Green,
 	enEffect_Knight_Ult_Blue,
 	enEffect_Knight_Ult_Yellow,
 	enEffect_Knight_Ult_Aura,
-	enEffect_Knight_Death_Num
-
+	enEffect_Knight_GetPower,
+	enEffect_Knight_PowerUP,
+	enEffect_Knight_GetHoimi,
+	enEffect_Neutral_Enemy_Death,
+	enEffect_Neutral_Enemy_head_butt,
+	enEffect_Num
 };
 

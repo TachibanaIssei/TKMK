108d68708c5866dc87fd39bedc75e77a98511687
azuki <yuhi55.k@gmail.com>
2023-05-09 20:11:26 +0900

    新しいエフェクトステート追加

--- TKMK/Game/Effect.h
+++ TKMK/Game/Effect.h
@@ -6,6 +6,7 @@ enum EnEFK {
 	enEffect_Knight_Ult_Green,
 	enEffect_Knight_Ult_Blue,
 	enEffect_Knight_Ult_Yellow,
+	enEffect_Knight_Ult_Delete_Blue,
 	enEffect_Knight_Ult_Aura,
 	enEffect_Knight_Attack,
 	/*enEffect_Knight_Atk_Red,

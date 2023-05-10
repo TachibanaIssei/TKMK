c0497a63b56c6d391e04b72aadf64b2a3554abe1
azuki <yuhi55.k@gmail.com>
2023-05-10 20:40:30 +0900

    デリートするときに剣士のフラグを直すようにした

--- TKMK/Game/WizardUlt.cpp
+++ TKMK/Game/WizardUlt.cpp
@@ -3,6 +3,7 @@
 #include "Player.h"
 #include "KnightAI.h"
 #include "Actor.h"
+#include "Game.h"
 
 WizardUlt::WizardUlt()
 {
@@ -12,6 +13,10 @@ WizardUlt::WizardUlt()
 WizardUlt::~WizardUlt()
 {
 	DeleteGO(UltCollision);
+	// 全てを終わらせる！！！！！！！
+	m_game->SetStopFlag(false);
+	//m_targetActor->UltEnd();
+	m_CreatMeActor->UltEnd();
 }
 
 bool WizardUlt::Start()
@@ -23,20 +28,36 @@ bool WizardUlt::Start()
 	//攻撃対象のキャラの名前
 	m_targetActor = FindGO<Actor>(SetTargetActorName());
 
+	Thunder = NewGO<EffectEmitter>(0);
+	Thunder->Init(enEffect_Knight_Thunder);
+
+	Vector3 ThunderPos = m_position;
+	ThunderPos.y = 0.0f;
+	Thunder->SetPosition(ThunderPos);
+	Thunder->SetScale(Vector3::One * 5.0f);
+	Thunder->Play();
 	return true;
 }
 
 void WizardUlt::Update()
 {
-	m_timer += g_gameTime->GetFrameDeltaTime();
-	//タイマーが３より大きくなったら
-	if (m_timer > 3)
+	if (Thunder->IsPlay() == false)
 	{
 		//攻撃対象のキャラにダメージを与える処理
 		Damege();
 		//自身を消す
 		DeleteGO(this);
 	}
+
+	m_timer += g_gameTime->GetFrameDeltaTime();
+	//タイマーが３より大きくなったら
+	//if (m_timer > 3)
+	//{
+	//	//攻撃対象のキャラにダメージを与える処理
+	//	Damege();
+	//	//自身を消す
+	//	DeleteGO(this);
+	//}
 
 	Move();
 }
@@ -54,18 +75,18 @@ void WizardUlt::Damege()
 	int Randam = rand() % 100;
 	int Damege = 0;
 	//5分の1の確率で即死
-	if (Randam < 20)
+	if (Randam > 0)
 	{
 		Damege = 1000;
 		//攻撃対象のキャラにダメージを与える
 		m_targetActor->Dameged(Damege, m_CreatMeActor);
 		int downlevel = 1;
 		//必殺技を打ったキャラのレベルを下げる
-		m_CreatMeActor->levelDown(
+		/*m_CreatMeActor->levelDown(
 			m_CreatMeActor->GetLvUPStatus(), 
 			m_CreatMeActor->GetStatus(), 
 			m_CreatMeActor->GetLevel(), 
-			downlevel);
+			downlevel);*/
 	}
 
 	

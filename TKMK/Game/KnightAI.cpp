4288b85471c328f063b3ec6518f53c6d36a72e79
azuki <yuhi55.k@gmail.com>
2023-05-12 17:41:18 +0900

    判定を増やした

--- TKMK/Game/KnightAI.cpp
+++ TKMK/Game/KnightAI.cpp
@@ -1008,13 +1008,15 @@ void KnightAI::OnAnimationEvent(const wchar_t* clipName, const wchar_t* eventNam
 		//地上にいるキャラをカウントする
 		for (auto actor : m_game->GetActors())
 		{
-			if (GetName() == actor->GetName()) {
+			if (GetName() == actor->GetName() || actor->GetGroundChackflag() == true) {
 				continue;
 			}
 
 			if (actor->IsGroundIn() == true)
 			{
 				m_OnGroundCharCounter++;
+				//このキャラはグラウンドにいる
+				actor->ChangeGroundChackflag(true);
 			}
 		}
 		//必殺技の当たり判定のクラスを作成

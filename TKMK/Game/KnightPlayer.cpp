1370beeaf8a94f232df5758e54fe30d4ecc70273
azuki <yuhi55.k@gmail.com>
2023-05-26 17:52:51 +0900

    スキルステートの処理。回避に使えるボタン増やした

--- TKMK/Game/KnightPlayer.cpp
+++ TKMK/Game/KnightPlayer.cpp
@@ -237,7 +237,7 @@ void KnightPlayer::Update()
 			//スキル使用中なら
 			if (SkillState == true) {
 				//スキルステート
-				m_charState = enCharState_Skill;
+				//m_charState = enCharState_Skill;
 				//移動処理を行う(直線移動のみ)。
 				MoveStraight();
 			}
@@ -401,7 +401,7 @@ void KnightPlayer::Attack()
 		}
 		
 		SkillEndFlag = true;
-		
+		m_charState = enCharState_Skill;
 		pushFlag = true;
 		SkillState = true;
 	}
@@ -507,11 +507,13 @@ void KnightPlayer::Avoidance()
 	}
 	//RBボタンが押されたら。
 	//回避
-	if (pushFlag == false && AvoidanceEndFlag == false && AvoidanceFlag == false && g_pad[0]->IsTrigger(enButtonRB1)) {
-		//回避ステート
-		AnimationMove(AvoidanceSpeed);
-		pushFlag = true;
-		AvoidanceFlag = true;
+	if (pushFlag == false && AvoidanceEndFlag == false && AvoidanceFlag == false) {
+		if (g_pad[0]->IsTrigger(enButtonRB1) || g_pad[0]->IsTrigger(enButtonLB1)) {
+			//回避ステート
+			AnimationMove(AvoidanceSpeed);
+			pushFlag = true;
+			AvoidanceFlag = true;
+		}
 	}
 }
 

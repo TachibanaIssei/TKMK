d6bcf0e040836c80bba33b324ad6fb735b3f9b92
Tachibana <tatibana.issei.kawahara@gmail.com>
2023-08-25 23:38:21 +0900

    FontRenderのリファクタリング終了

--- TKMK/k2EngineLow/FontRender.cpp
+++ TKMK/k2EngineLow/FontRender.cpp
@@ -1,6 +1,14 @@
 #include "k2EngineLowPreCompile.h"
 #include "FontRender.h"
 
+nsK2EngineLow::FontRender::FontRender()
+{
+}
+
+nsK2EngineLow::FontRender::~FontRender()
+{
+}
+
 void nsK2EngineLow::FontRender::Draw(RenderContext& rc)
 {
 	g_renderingEngine->AddFontList(this);
